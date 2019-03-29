450ea9e514dc36d20c041443907e8841db08ed94
Narutou50 <negro.diaz.27@gmail.com>
2019-03-29 18:48:43 -0300

    punto 1 corregido

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -1,13 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 #define N 4
 #define M 5
-Int f,c;
-Double mt[N][M];
-…
-for(f = 0;f<N; f++)
-{
-	 for(f = 0;f<N; f++)
+
+int main(void){
+	Int f,c;
+	Double mt[N][M];
+	…
+	for(f = 0;f<N; f++)
 	{
-		cprintf(“%lf ”, mt[f][c]);
+		 for(c = 0;f<M; f++)
+		{
+			cprintf(“%lf ”, mt[f][c]);
+		}
+		cprintf(“\n”);
 	}
-	cprintf(“\n”);
-}
+};
